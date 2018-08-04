eda4bbd09d98b5ccc58e85e324453178ccf5ccd4
Donte Small <small.donte@yahoo.com>
2018-08-04 17:24:52 -0400

    Fixed get method, Passed two test cases,Need to figure out solution for get_item test case

--- Hashmap.py
+++ Hashmap.py
@@ -3,19 +3,31 @@ from LinkedList import linkedList,Node
 
 class hashcontainer_of_hashed_elements(object):
     def __init__(self):
-        self.size = 8
+        self.size = 4
         self.container_of_hashed_elements= [None] * self.size
 
     def __set__(self,key, value):
-
+        # gets the key
         indx_of_key_value = self.__get__(key)
+
+        #creates key + value pairs
         value = [key,value]
 
         if self.container_of_hashed_elements[indx_of_key_value] is None:
+
+            #sets the empty container to list of "key" "value"
             self.container_of_hashed_elements[indx_of_key_value] = list([value])
+            assigned_value = self.container_of_hashed_elements[indx_of_key_value]
+            return assigned_value
+
         else:
+            # if not empty, check if element at indx[0] equals key
+            print self.container_of_hashed_elements[indx_of_key_value],
             for pair in self.container_of_hashed_elements[indx_of_key_value]:
+
                 if pair[0] == key:
+                    print(pair[0])
+                    # if so, then the element at indx[1] corresponds to its value
                     pair[1] = value
                     return True
 
@@ -24,12 +36,25 @@ class hashcontainer_of_hashed_elements(object):
     def has_key(self,key):
         pass
 
-
     def __get__(self,key):
         hash = 0
-        for char in str(key):
-            hash += ord(char)
-        return hash % self.size
+        for indx_value in str(key):
+            hash += ord(indx_value)
+            calculatated_values = hash % self.size
+
+        return calculatated_values
+    def get_item(self,key):
+        key_value = self.__get__(key)
+        if self.container_of_hashed_elements[key_value] is not None:
+
+            for pair in self.container_of_hashed_elements[key_value]:
+
+                if pair[0] == key:
+                    return pair[1]
+        return None
+
+
+
 
 
     def delete(self,key):
@@ -47,8 +72,7 @@ def main():
     h.__set__(10,"Nope")
     h.__set__(13,"Almost")
     h.__set__(12,"Gone")
+    h.get_item(12)
     print(h)
     #h.delete(13)
-    print("Was removed")
-    print(h)
-main()
+    print(h.get_item(10))
--- hashmap_unit_tests.py
+++ hashmap_unit_tests.py
@@ -1,23 +1,31 @@
 import unittest
 from LinkedList import Node,linkedList
-from Hashmap import hashmap
+from Hashmap import hashcontainer_of_hashed_elements
 
 class hashmap_test(unittest.TestCase):
     def setUp(self):
-        hash_map = hashmap()
+        self.hash_map = hashcontainer_of_hashed_elements()
 
 
     def test_set_element_in_hash_map(self):
-        set_hash = hashmap()
-        self.assertTrue(set_hash.__set__("key1","val1"))
+        set_hash = hashcontainer_of_hashed_elements()
+        result= set_hash.__set__("key1","val1")
+        self.assertTrue("key1" and "val1" in result[0])
+
+    def test_element_is_not_in_hash_map(self):
+        self.assertTrue("Biggie" is not self.hash_map)
+
+
 
     def test_set_two_elments(self):
             pass
 
-    # def test_get_one_element_in_hash_map(self):
-    #     get_hash = hashmap()
-    #
-    #     self.assertEqual(get_hash.___get__('2'),2)
+    def test_get_one_element_in_hash_map(self):
+        get_hash = hashcontainer_of_hashed_elements()
+        result= get_hash.__set__(2,"val1")
+        result2=result.get_item(2)
+
+        self.assertTrue(result2 == 2)
 
 
     def test_count_is_correct(self):
