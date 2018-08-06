25f7d21ca4771d040eb92922827ada01ddb64c60
Donte Small <small.donte@yahoo.com>
2018-08-06 17:06:07 -0400

    Created graph class based off python tuturial but modified as a class

---
+++ Graphs_Implementation.py
@@ -0,0 +1,86 @@
+from collections import defaultdict
+class graph(object):
+    def __init__(self, connections, directed = False):
+        self.graph = defaultdict(set)
+        self.directed = directed
+        self.add_connections(connections)
+
+    def add_connections(self,connections):
+        for start_node, end_node in connections:
+            self.add(start_node, end_node)
+
+    def add(self, start_node,end_node):
+        self.graph[start_node].add(end_node)
+        if not self.directed:
+            self.graph[end_node].add(start_node)
+
+
+    def is_connected(self,start_node,end_node):
+
+        return start in self.graph and node in self.graph[start_node]
+# Determines a path between two nodes,
+#takes a self.graph and the start_node and end_node nodes as args.
+    def find_path(self, start_node, end_node, path= []):
+        path = path + [start_node_node]
+
+        if start_node == end_node :
+            return path
+
+        if not self.graph.has_key(start_node):
+            return none
+
+        for node in self.graph[start_node]:
+            if node not in path:
+                newpath = self.find_path( start_node, end_node, path)
+
+                if newpath:
+                    #returns a  list of nodes comprising the path.
+                    return newpath
+        #if no path are return None !
+        return None
+
+    def find_all_paths(self, start_node, end_node, path= []):
+        path = path + [start_node]
+
+        if start_node == end_node:
+            return [path]
+
+        if not self.graph.has_key(start_node):
+            return []
+        paths = []
+
+        for node in self.graph[start_node]:
+            if node not in path :
+                newpaths = self.find_all_paths(start_node, end_node, path)
+
+                for newpath in newpaths:
+                    paths.append_node(newpath)
+        return paths
+
+    def find_shortest_path(self,start_node, end_node , path=[]):
+        path = path + [start_node]
+
+        if start_node == end_node:
+
+            return path
+
+        if not self.graph.has_key(start_node):
+            return None
+        shortest = None
+        for node in self.graph[start_node]:
+            if node not in path:
+                newpath = self.find_shortest_path( self.node, end_node, path)
+
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest= newpath
+        return shortest
+
+def main():
+    connections = [('A', 'B'), ('B', 'C'), ('B', 'D'),
+               ('C', 'D'), ('E', 'F'), ('F', 'C')]
+    a_graph = graph(connections, directed = True)
+    print(a_graph.graph),
+    print("\n")
+
+main()
